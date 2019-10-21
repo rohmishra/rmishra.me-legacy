ca2be62cc02dba0a052188ad6ca70b5f7cdc22a7
Rohan Mishra <rohmish26@gmail.com>
2019-10-21 07:50:03 -0400

    Animated NavBar

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-console.log ("Test Message?");
-let x = "hello";
-console.log(x);
-
-let start = () => {
-  console.log("Start");
+const heroSection = document.querySelector("#hero"); // Select the hero section
+const navBar = document.querySelector("nav"); // Seclect the navbar
+const options = {
+  root: null,
+  threshold: 0,
+  rootMargin: "-30px"
 };
+const heroSpecialObs = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(() => toggleUI()); // run toggle when intersecting changes.
+  },
+  options
+);
 
-start();
+heroSpecialObs.observe(heroSection); //Ask the heroSection observer to observe hero section
+
+const toggleUI = () => {
+  navBar.classList.toggle("heroSpecial");
+};
